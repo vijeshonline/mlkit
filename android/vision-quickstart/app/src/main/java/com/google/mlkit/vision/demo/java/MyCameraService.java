c668f3efb1c6f1adc888c2a3ff640a35e8ad26ca
Vijesh <vijeshkumarv@gmail.com>
2021-05-25 19:09:57 +0530

    Service added
    Service added to support pose detection in background service.

--- android/vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/java/MyCameraService.java
+++ android/vision-quickstart/app/src/main/java/com/google/mlkit/vision/demo/java/MyCameraService.java
@@ -1,54 +1,246 @@
 package com.google.mlkit.vision.demo.java;
 
+import android.app.Service;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
+
 import android.content.Intent;
+
+import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
+import android.util.Size;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.NotificationCompat;
 
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.HasDefaultViewModelProviderFactory;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LifecycleService;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStore;
+import androidx.lifecycle.ViewModelStoreOwner;
+
+import com.google.mlkit.common.MlKitException;
+import com.google.mlkit.vision.demo.CameraXViewModel;
+import com.google.mlkit.vision.demo.GraphicOverlay;
 import com.google.mlkit.vision.demo.R;
+import com.google.mlkit.vision.demo.VisionImageProcessor;
+import com.google.mlkit.vision.demo.java.posedetector.PoseDetectorProcessor;
+import com.google.mlkit.vision.demo.preference.PreferenceUtils;
+import com.google.mlkit.vision.pose.PoseDetectorOptionsBase;
+
+
+import static com.google.mlkit.vision.demo.preference.PreferenceUtils.getCameraXTargetResolution;
 //import android.support.annotation.Nullable;
 //import android.support.v4.app.NotificationCompat;
 
-public class MyCameraService extends Service {
+public class MyCameraService extends LifecycleService implements ViewModelStoreOwner, HasDefaultViewModelProviderFactory {
     public static final String CHANNEL_ID = "ForegroundServiceChannel";
+    private static final String TAG = "MyCameraService";
+
+    private View mView;
+    private PreviewView previewView;
+    private GraphicOverlay graphicOverlay;
+    @Nullable private ProcessCameraProvider cameraProvider;
+    @Nullable private Preview previewUseCase;
+    @Nullable private ImageAnalysis analysisUseCase;
+    @Nullable private VisionImageProcessor imageProcessor;
+    private boolean needUpdateGraphicOverlayImageSourceInfo;
+    private static final String POSE_DETECTION = "Pose Detection";
+    private String selectedModel = POSE_DETECTION;
+    private int lensFacing = CameraSelector.LENS_FACING_FRONT;
+    private CameraSelector cameraSelector;
+
+    final ViewModelStore mViewModelStore = new ViewModelStore();
+    ViewModelProvider.Factory mFactory;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        //Toast.makeText(this, "Service was Created", Toast.LENGTH_LONG).show();
+        Log.i("MyCameraService","VIJESH: onCreate()");
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        View mView = inflater.inflate(R.layout.activity_vision_camerax_live_preview, null);
+//        ViewGroup.LayoutParams params = new WindowManager.LayoutParams(
+//                WindowManager.LayoutParams.MATCH_PARENT,
+//                WindowManager.LayoutParams.WRAP_CONTENT,
+//                WindowManager.LayoutParams.TYPE_APPLICATION_PANEL,
+//                0,
+//                PixelFormat.TRANSLUCENT);
+//        wm.addView(mView, params);
+
+        previewView = mView.findViewById(R.id.preview_view);
+        if (previewView == null) {
+            Log.i("MyCameraService", "VIJESH previewView is null");
+        }
+        graphicOverlay = mView.findViewById(R.id.graphic_overlay);
+        if (graphicOverlay == null) {
+            Log.i("MyCameraService", "VIJESH graphicOverlay is null");
+        }
+
+        cameraSelector = new CameraSelector.Builder().requireLensFacing(lensFacing).build();
+
+        getLifecycle().addObserver(new LifecycleEventObserver() {
+            @Override
+            public void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event) {
+                if (source.getLifecycle().getCurrentState() == Lifecycle.State.DESTROYED) {
+                    mViewModelStore.clear();
+                    source.getLifecycle().removeObserver(this);
+                }
+            }
+        });
+
+        new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()))
+                .get(CameraXViewModel.class)
+                .getProcessCameraProvider()
+                .observe(
+                         this,
+                        provider -> {
+                            cameraProvider = provider;
+                            bindAllCameraUseCases();
+                        });
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+        Log.i("MyCameraService", "VIJESH: onStartCommand");
         String input = intent.getStringExtra("inputExtra");
         createNotificationChannel();
         Intent notificationIntent = new Intent(this, CameraXLivePreviewActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,
                 0, notificationIntent, 0);
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Foreground Service")
+                .setContentTitle("Gesture Service")
                 .setContentText(input)
                 .setSmallIcon(R.drawable.logo_mlkit)
                 .setContentIntent(pendingIntent)
                 .build();
-        startForeground(1, notification);
+        startForeground(1555, notification);
         //do heavy work on a background thread
         //stopSelf();
+        Log.i("MyCameraService", "VIJESH: onStartCommand END");
         return START_NOT_STICKY;
     }
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.i("MyCameraService","VIJESH: onDestroy()");
     }
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        super.onBind(intent);
+        Log.i("MyCameraService", "VIJESH: onBind()");
         return null;
     }
+    private void bindAllCameraUseCases() {
+        if (cameraProvider != null) {
+            // As required by CameraX API, unbinds all use cases before trying to re-bind any of them.
+            cameraProvider.unbindAll();
+            //bindPreviewUseCase();
+            bindAnalysisUseCase();
+        }
+    }
+
+
+    private void bindAnalysisUseCase() {
+        if (cameraProvider == null) {
+            return;
+        }
+        if (analysisUseCase != null) {
+            cameraProvider.unbind(analysisUseCase);
+        }
+        if (imageProcessor != null) {
+            imageProcessor.stop();
+        }
+
+        try {
+            switch (selectedModel) {
+                case POSE_DETECTION:
+                    Log.w(TAG, "VIJESH : POSE_DETECTION");
+                    PoseDetectorOptionsBase poseDetectorOptions =
+                            PreferenceUtils.getPoseDetectorOptionsForLivePreview(this);
+                    boolean shouldShowInFrameLikelihood =
+                            PreferenceUtils.shouldShowPoseDetectionInFrameLikelihoodLivePreview(this);
+                    boolean visualizeZ = PreferenceUtils.shouldPoseDetectionVisualizeZ(this);
+                    boolean rescaleZ = PreferenceUtils.shouldPoseDetectionRescaleZForVisualization(this);
+                    boolean runClassification = PreferenceUtils.shouldPoseDetectionRunClassification(this);
+                    imageProcessor =
+                            new PoseDetectorProcessor(
+                                    this, poseDetectorOptions, shouldShowInFrameLikelihood, visualizeZ, rescaleZ,
+                                    runClassification, /* isStreamMode = */true);
+                    break;
+                default:
+                    throw new IllegalStateException("Invalid model name");
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Can not create image processor: " + selectedModel, e);
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Can not create image processor: " + e.getLocalizedMessage(),
+                    Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        ImageAnalysis.Builder builder = new ImageAnalysis.Builder();
+        Size targetResolution = getCameraXTargetResolution(this, lensFacing);
+        if (targetResolution != null) {
+            builder.setTargetResolution(targetResolution);
+        }
+        analysisUseCase = builder.build();
+
+        needUpdateGraphicOverlayImageSourceInfo = true;
+        analysisUseCase.setAnalyzer(
+                // imageProcessor.processImageProxy will use another thread to run the detection underneath,
+                // thus we can just runs the analyzer itself on main thread.
+                ContextCompat.getMainExecutor(this),
+                imageProxy -> {
+                    if (needUpdateGraphicOverlayImageSourceInfo) {
+                        boolean isImageFlipped = lensFacing == CameraSelector.LENS_FACING_FRONT;
+                        int rotationDegrees = imageProxy.getImageInfo().getRotationDegrees();
+                        if (rotationDegrees == 0 || rotationDegrees == 180) {
+                            graphicOverlay.setImageSourceInfo(
+                                    imageProxy.getWidth(), imageProxy.getHeight(), isImageFlipped);
+                        } else {
+                            graphicOverlay.setImageSourceInfo(
+                                    imageProxy.getHeight(), imageProxy.getWidth(), isImageFlipped);
+                        }
+                        needUpdateGraphicOverlayImageSourceInfo = false;
+                    }
+                    try {
+                        imageProcessor.processImageProxy(imageProxy, graphicOverlay);
+                    } catch (MlKitException e) {
+                        Log.e(TAG, "Failed to process image. Error: " + e.getLocalizedMessage());
+                        Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+
+        cameraProvider.bindToLifecycle(/* lifecycleOwner= */ (LifecycleOwner) this, cameraSelector, analysisUseCase);
+    }
+
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel serviceChannel = new NotificationChannel(
@@ -59,5 +251,17 @@ public class MyCameraService extends Service {
             NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(serviceChannel);
         }
+    }
+
+    @NonNull
+    @Override
+    public ViewModelStore getViewModelStore() {
+        return mViewModelStore;
+    }
+
+    @NonNull
+    @Override
+    public ViewModelProvider.Factory getDefaultViewModelProviderFactory() {
+        return mFactory != null ? mFactory : (mFactory = new ViewModelProvider.AndroidViewModelFactory(getApplication()));
     }
 }
